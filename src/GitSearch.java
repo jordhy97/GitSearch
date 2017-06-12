45ff5740799468f271d9ef8cbb28c6b1d7497cd9
jordhy97 <jordhy.fernando@gmail.com>
2017-06-12 12:16:34 +0700

    Added GitSearch (Main Class)

---
+++ src/GitSearch.java
@@ -0,0 +1,19 @@
+import com.gitsearch.view.MainView;
+
+/*
+ * Nama File          : GitSearch.java
+ * Tanggal dibuat     : 11/06/17
+ * Tanggal perubahan  : 12/06/17
+ */
+
+
+/**
+ * Kelas GitSearch yang merupakan program utama.
+ *
+ * @author Jordhy Fernando
+ */
+public class GitSearch {
+  public static void main(String[] args){
+    MainView view = new MainView();
+  }
+}
